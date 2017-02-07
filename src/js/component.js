54ffcecaa566bcff62709062556745d5218d5cbe
bangdasom <creativesomii@gmail.com>
2017-02-08 00:54:03 +0900

    2017.02.07 직접입력 삭제

--- src/js/component.js
+++ src/js/component.js
@@ -13,24 +13,16 @@ $('.tab .nav a').on('click', function(e) {
 });
 
 /* 셀렉트박스 직접입력 시 */
-$(document).on("change", ".select select", function() {
+/*$(document).on("change", ".select select", function() {
     var dir = $(this).find("option:selected").val();
 
     if(dir == "direct") {
         $(this).find("option.dir").text("");
         $(this).closest(".select").addClass("active").find(".dir-input input").focus();
 
-        /*$(document).on("change", ".select select", function() {
-          console.log("재클릭");
-            
-          if($(".select").hasClass("active")) {
-              $(this).find("option.dir").text("직접입력");
-          }
-        });*/
-
     } else  {
         $(this).closest(".select").removeClass("active").find(".dir-input input").val("");
         $(this).find("option.dir").text("직접입력");
     }
-});
+});*/
 
