ef865973752850b1ec09d32f658b1241e08a925d
bangdasom <creativesomii@gmail.com>
2017-02-08 18:41:57 +0900

    2017.02.08 상품메인 대분류

--- dist/js/component.js
+++ dist/js/component.js
@@ -9,15 +9,3 @@ $(".tab .nav a").on("click", function(e) {
     $(this).parent().addClass("active").siblings().removeClass("active");
     e.preventDefault();
 });
-
-/* 셀렉트박스 직접입력 시 */
-$(document).on("change", ".select select", function() {
-    var dir = $(this).find("option:selected").val();
-    if (dir == "direct") {
-        $(this).find("option.dir").text("");
-        $(this).closest(".select").addClass("active").find(".dir-input input").focus();
-    } else {
-        $(this).closest(".select").removeClass("active").find(".dir-input input").val("");
-        $(this).find("option.dir").text("직접입력");
-    }
-});
